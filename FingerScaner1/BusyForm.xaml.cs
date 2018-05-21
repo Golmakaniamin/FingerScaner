2dd8738607d43fee1d12c8e1f292f16208ee37e5
Golmakaniamin <34438263+Golmakaniamin@users.noreply.github.com>
2018-05-21 12:28:14 +0430

    Amin Golmakani

---
+++ FingerScaner1/BusyForm.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Neurotec.Gui
+{
+    /// <summary>
+    /// Interaction logic for BusyForm.xaml
+    /// </summary>
+    public partial class BusyForm : Window
+    {
+        private DoWorkEventHandler callback;
+        private object argument;
+        private RunWorkerCompletedEventArgs results;
+        private Exception error;
+        protected BackgroundWorker _worker = new BackgroundWorker();
+
+        private BusyForm(string title, DoWorkEventHandler callback)
+            : this(title, callback, false, null, null)
+        {
+        }
+
+        private BusyForm(string title, DoWorkEventHandler callback, object args)
+            : this(title, callback, false, args, null)
+        {
+        }
+
+        private BusyForm(string title, DoWorkEventHandler callback, bool reportsProgress)
+            : this(title, callback, reportsProgress, null, null)
+        {
+        }
+
+        private BusyForm(string title, DoWorkEventHandler callback, bool reportsProgress, object args, EventHandler cancelHandler)
+        {
+            InitializeComponent();
+
+            //if (!reportsProgress)
+            //    progressBar.Style = ProgressBarStyle.Marquee;
+
+            SetExecutionText(title);
+            this.callback = callback;
+            argument = args;
+            _worker.WorkerReportsProgress = reportsProgress;
+            _worker.DoWork += BusyForm_DoWork;
+            _worker.RunWorkerCompleted += BusyForm_RunWorkerCompleted;
+            _worker.ProgressChanged += BusyForm_ProgressChanged;
+
+            //if (cancelHandler != null)
+            //{
+            //    _worker.WorkerSupportsCancellation = true;
+            //    btnCancel.Click += cancelHandler;
+            //    btnCancel.Click += new EventHandler(PostOnCancelClick);
+            //    btnCancel.Enabled = true;
+            //    btnCancel.Visible = true;
+            //}
+        }
+
+        void PostOnCancelClick(object sender, EventArgs e)
+        {
+            btnCancel.IsEnabled = false;
+            //btnCancel.Refresh();
+        }
+
+        public static RunWorkerCompletedEventArgs RunLongTask(string title, DoWorkEventHandler callback)
+        {
+            return RunLongTask(title, callback, false);
+        }
+
+        public static RunWorkerCompletedEventArgs RunLongTask(string title, DoWorkEventHandler callback, bool reportsProgress)
+        {
+            BusyForm frmLongTask = new BusyForm(title, callback, reportsProgress);
+            frmLongTask.ShowDialog();
+            if (frmLongTask.error != null)
+            {
+                throw frmLongTask.error;
+            }
+            return frmLongTask.results;
+        }
+
+        public static RunWorkerCompletedEventArgs RunLongTask(string title, DoWorkEventHandler callback, bool reportsProgress, object args, EventHandler cancelHandler)
+        {
+            BusyForm frmLongTask = new BusyForm(title, callback, reportsProgress, args, cancelHandler);
+            frmLongTask.ShowDialog();
+            if (frmLongTask.error != null)
+            {
+                throw frmLongTask.error;
+            }
+            return frmLongTask.results;
+        }
+
+        private void BusyForm_Shown(object sender, EventArgs e)
+        {
+            _worker.RunWorkerAsync(argument);
+        }
+
+        #region Background worker
+        private void BusyForm_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                if (callback != null)
+                {
+                    callback(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                error = ex;
+            }
+        }
+
+        private void BusyForm_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string text = e.UserState as string;
+            if (text != null)
+            {
+                SetExecutionText(text);
+            }
+
+            SetExecutionValue(e.ProgressPercentage);
+        }
+
+        private void BusyForm_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            results = e;
+            Close();
+        }
+        #endregion
+
+        #region Setters/Getters
+        public void SetExecutionText(string text)
+        {
+            //try
+            //{
+            //    if (lblOperation.InvokeRequired)
+            //    {
+            //        lblOperation.Invoke((MethodInvoker)delegate()
+            //        {
+            //            lblOperation.Text = text;
+            //        });
+            //    }
+            //    else
+            //    {
+            //        lblOperation.Text = text;
+            //    }
+            //}
+            //finally
+            //{
+            //}
+        }
+
+        public void SetExecutionValue(int value)
+        {
+            //try
+            //{
+            //    if (progressBar.InvokeRequired)
+            //    {
+            //        progressBar.Invoke((MethodInvoker)delegate()
+            //        {
+            //            progressBar.Value = value;
+            //        });
+            //    }
+            //    else
+            //    {
+            //        progressBar.Value = value;
+            //    }
+            //}
+            //finally
+            //{
+            //}
+        }
+        #endregion
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FingerScaner1/MainForm.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Neurotec.Biometrics;
+using Neurotec.Gui;
+
+namespace FingerScaner1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainForm : Window
+    {
+        Nffv _engine;
+        string _userDatabaseFile;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        internal class EnrollmentResult
+        {
+            public NffvStatus engineStatus;
+            public NffvUser engineUser;
+        };
+
+        private void doEnroll(object sender, DoWorkEventArgs args)
+        {
+            EnrollmentResult enrollmentResults = new EnrollmentResult();
+            enrollmentResults.engineUser = _engine.Enroll(20000, out enrollmentResults.engineStatus);
+            args.Result = enrollmentResults;
+        }
+
+        internal class VerificationResult
+        {
+            public NffvStatus engineStatus;
+            public int score;
+        };
+
+        private void doVerify(object sender, DoWorkEventArgs args)
+        {
+            VerificationResult verificationResult = new VerificationResult();
+            verificationResult.score = _engine.Verify((NffvUser)args.Argument, 20000, out verificationResult.engineStatus);
+            args.Result = verificationResult;
+        }
+
+        private void CancelScanningHandler(object sender, EventArgs e)
+        {
+            _engine.Cancel();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            RunWorkerCompletedEventArgs taskResult = BusyForm.RunLongTask("Waiting for fingerprint ...", new DoWorkEventHandler(doEnroll), false, null, new EventHandler(CancelScanningHandler));
+            EnrollmentResult enrollmentResult = (EnrollmentResult)taskResult.Result;
+            if (enrollmentResult.engineStatus == NffvStatus.TemplateCreated)
+            {
+                NffvUser engineUser = enrollmentResult.engineUser;
+                string userName = "1";
+                if (userName.Length <= 0)
+                {
+                    userName = engineUser.Id.ToString();
+                }
+
+                //_userDB.Add(new UserRecord(engineUser.Id, userName));
+                try
+                {
+                    //_userDB.WriteToFile(_userDatabaseFile);
+                }
+                catch { }
+
+                System.IntPtr a1;
+                System.Windows.Forms.PictureBox po = new PictureBox();
+                a1 = engineUser.GetHBitmap();
+                
+
+                //pbExtractedImage.Image = engineUser.GetBitmap();
+
+                //lbDatabase.Items.Add(new CData(engineUser, userName));
+                //lbDatabase.SelectedIndex = lbDatabase.Items.Count - 1;
+            }
+            else
+            {
+                NffvStatus engineStatus = enrollmentResult.engineStatus;
+                //MessageBox.Show(string.Format("Enrollment was not finished. Reason: {0}", engineStatus));
+            }
+        }
+
+    }
+
+    class CData : IDisposable
+    {
+        private NffvUser _engineUser;
+        private Bitmap _image;
+        private string _name;
+
+        public CData(NffvUser engineUser, string name)
+        {
+            _engineUser = engineUser;
+            _image = engineUser.GetBitmap();
+            _name = name;
+        }
+
+        public NffvUser EngineUser
+        {
+            get
+            {
+                return _engineUser;
+            }
+        }
+
+        public Bitmap Image
+        {
+            get
+            {
+                return _image;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _engineUser.Id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (_image != null)
+            {
+                _image.Dispose();
+                _image = null;
+            }
+        }
+
+        #endregion
+    }
+}
